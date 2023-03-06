c29306d6f7096c3e1df363d09ea15d38e9b0b5b9
shivam saini <60741497+shivam199063@users.noreply.github.com>
2023-03-06 09:14:08 +0530

    update connect.py

--- connect.py
+++ connect.py
@@ -1,6 +1,6 @@
 import openai
 while True:
-    prompt_value=input("Prompt :>> ")
+    prompt_value=input("\nPrompt :>> ")
     if(prompt_value=="exit"):            #if you want to close Press "exit"
         break
     else:
@@ -8,4 +8,4 @@ while True:
         openai.api_key="key_vaalue"  
         output=openai.Completion.create(prompt=prompt_value,model="text-davinci-003",max_tokens=2000)
         output_text=output["choices"][0]["text"]
-        print(output_text)
+        print(output_text[1:)
